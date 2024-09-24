c5a1585c29b8b5104539ce913d88da96a1631f70
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2024-09-25 01:57:46 +0900

    worker 오류 수정

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ import './search-group.js';
 
 import './top-banner--pc.js';
 import './write-list-popup.js';
-import './timer.js';
+// import './timer.js';
 import './worker.js';
 
 import './swiper/banner-slide.js';
@@ -35,3 +35,30 @@ const images = require.context(
   false,
   /\.(png|jpg|jpeg|gfif|svg)$/
 );
+
+export const worker = new Worker(new URL('./worker.js', import.meta.url));
+
+const timeBadges = document.querySelectorAll('.badge--time');
+
+timeBadges.forEach((badge) => {
+  const hours = new Date().getHours().toString().padStart(2, '0');
+  const minutes = new Date().getMinutes().toString().padStart(2, '0');
+  const seconds = new Date().getSeconds().toString().padStart(2, '0');
+  badge.innerText = `${hours}:${minutes}:${seconds} 남음`;
+});
+
+worker.postMessage({ type: 'start' });
+
+worker.onmessage = function (e) {
+  const { hours, minutes, seconds } = e.data;
+  if (hours === '00' && minutes === '00' && seconds === '00') {
+    timeBadges.forEach((badge) => {
+      badge.innerText = `마감`;
+    });
+    this.postMessage({ type: 'end' });
+  } else {
+    timeBadges.forEach((badge) => {
+      badge.innerText = `${hours}:${minutes}:${seconds} 남음`;
+    });
+  }
+};
