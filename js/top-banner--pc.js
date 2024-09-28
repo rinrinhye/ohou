1ce7dfc7c2faf05e7342d7e88d00cc94028aa6b7
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2024-09-28 21:30:02 +0900

    tab 바로가기 추가

--- js/top-banner--pc.js
+++ js/top-banner--pc.js
@@ -1,8 +1,6 @@
-import { addClassList, removeClassList } from './utils/classlist.js';
 import { currentInnerWidth } from './utils/resize.js';
 import { setPopupPosition } from './popup-transform.js';
 
-const header = document.querySelector('.header');
 const pcTopBanner = document.querySelector('.top-banner--pc');
 const pcTopBannerCloseButton = document.querySelector(
   '.top-banner__close-button--pc'
@@ -13,27 +11,7 @@ function closepcTopBanner() {
   const x = (currentInnerWidth - maxXLWidth) / 2;
 
   pcTopBanner.remove();
-  removeClassList(header, 'top-50');
   setPopupPosition(x, 0);
 }
 
-function togglepcTopBannerOnLoad() {
-  if (currentInnerWidth <= 1024) {
-    removeClassList(header, 'top-50');
-  }
-}
-
-function toggleHeaderClassListOnResize() {
-  if (currentInnerWidth >= 1024) {
-    const pcTopBanner = document.querySelector('.top-banner--pc');
-    if (pcTopBanner) {
-      addClassList(header, 'top-50');
-    }
-  } else {
-    removeClassList(header, 'top-50');
-  }
-}
-
 pcTopBannerCloseButton.addEventListener('click', closepcTopBanner);
-window.addEventListener('load', togglepcTopBannerOnLoad);
-window.addEventListener('resize', toggleHeaderClassListOnResize);
